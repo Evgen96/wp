3904134fbef7cebbef75f2a7a48322c735c48a10
Evgeny <p3000@outlook.com>
2015-03-15 16:42:14 +0500

    create master.blade 1.0

---
+++ app/Http/ViewComposers/ProfileComposer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BE
+ * Date: 14.03.2015
+ * Time: 5:18
+ */
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Users\Repository as UserRepository;
+
+class ProfileComposer {
+	/**
+	 * The user repository implementation.
+	 *
+	 * @var UserRepository
+	 */
+	protected $users;
+
+	/**
+	 * Create a new profile composer.
+	 *
+	 * @param  UserRepository  $users
+	 * @return void
+	 */
+	public function __construct(UserRepository $users)
+	{
+		// Dependencies automatically resolved by service container...
+		$this->users = $users;
+	}
+
+	/**
+	 * Bind data to the view.
+	 *
+	 * @param  View  $view
+	 * @return void
+	 */
+	public function compose(View $view)
+	{
+		$view->with('count', $this->users->count());
+	}
+
+}
--- resources/views/greeting.php
+++
@@ -1,7 +0,0 @@
-<!-- View stored in resources/views/greeting.php -->
-
-<html>
-<body>
-<h1>Hello, <?php echo $name; ?></h1>
-</body>
-</html>
